b66d977a87c82772c62558ffa824ff27de515baf
Taeyeong Kim <xodud7737@gmail.com>
2024-04-05 21:33:22 +0900

    Assingment

--- 5주차/matrix.c
+++ 5주차/matrix.c
@@ -4,6 +4,19 @@
 
 // 객체 지향 프로그래밍을 연습하기 위해 과제에서 요구하는 함수를 포함한 추가적인 함수를 만들었습니다.
 
+
+//*****************************************************************************************
+// 문제에서 행렬 A와 B가 정사각 행렬을 가정하는 것인지 모르겠습니다. 서로 다른 크기의 행과 열을 가진다면 Add,Sub 함수가 적용이 안 될것이고 행렬의 곱셈은 A의 열과 B의 행이 일치할 때만 가능할 텐데
+// 문제에서는 이에 대한 아무런 언급이 없어 모든 연산이 가능한 정사각 행렬을 가정하고 코드를 작성하였습니다.
+
+// 문제에서 요구하는 것은 A와 B의 연산이고 연산의 결과를 print_matrix로 출력하라고 요구하는데 문제에서 제시한 print_matrix() 함수는 A와 B의 행렬을 출력하는 함수이지
+// '연산 결과'를 출력하는 함수는 아니라고 생각됩니다. 연산의 결과를 A 행렬에 저장하거나 B 행렬에 저장하라는 말이 명시되어 있지 않아 연산의 결과를 출력해주는 함수'printer'를 임의로 만들었습니다.
+// 문제에서 요구하는 A와 B를 출력하는 함수도 구현했으나 연산의 결과는 printer()로 출력하였습니다. 제가 문제의 의도를 잘못 파악한거라면 죄송합니다!
+//*****************************************************************************************
+
+
+
+
 // 행렬을 만들기 위한 함수
 int ** make_array(int row_num,int column_num){
 
@@ -27,8 +40,11 @@ int ** make_array(int row_num,int column_num){
 }
 
 void my_info(){
-    print("[----- [김태영] [2023041012] -----]");
+
+
+    printf("[----- [김태영] [2023041012] -----]\n");
 }
+
 // 행렬을 랜덤 값으로 초기화 하기 위한 함수
 void put_random(int **array,int row, int column){
         ///////////////// (c) 랜덤 값으로 행렬 채우기 /////////////////
@@ -39,7 +55,7 @@ void put_random(int **array,int row, int column){
     /////////////////////////////////////////////////////////////////////////
 }
 
-// 행렬 출력을 간단히 하기 위한 보조함수 printer(굳이 함수로 안 만들어도 되지만 객체지향적 프로그래밍을 연습하고 싶어서 만들었습니다!)
+// 행렬 출력을 간단히 하기 위한 보조함수 printer
 void printer(int **array,int row_num,int column_num){ // 매개변수로 이중 포인터인 array와 행과 열의 정수 값을 받습니다.
     for(int i=0;i<row_num;++i){
         for(int j=0;j<column_num;++j){
@@ -59,74 +75,74 @@ void print_matrix(int **array_a, int ** array_b,int row,int col){ // 함수의 �
 /////////////////////////////////////////////////////////////////////////
 
 ///////////////// (e) addtion_matrix 구현 /////////////////
-void additon_matrix(int **array_a, int ** array_b,int row,int col){
-    int **temp = make_array(row,col);
+void additon_matrix(int **array_a, int ** array_b,int row,int col){ // 함수의 매개변수로 이중 포인터 변수인 행렬 A와 행렬 B를 받고 행과 열의 크기를 받습니다.
+    int **temp = make_array(row,col); // 새로운 이중 포인터 temp를 행렬 생성함수 make_array로 초기화 합니다.
     for (int i=0;i<row;++i){ // 행을 탐색하기 위한 for문
         for (int j=0;j<col;++j) // 열을 탐색하기 위한 for문
-            temp[i][j] = array_a[i][j] + array_b[i][j]; // 1~9중 랜덤 값을 array[i][[j]의 값으로 초기화
-    }
+            temp[i][j] = array_a[i][j] + array_b[i][j]; // 행렬의 각각의 요소의 서로 동일한 인덱스를 공유하는 요소와 더합니다. 그 결과를 2차원 배열 temp에 저장합니다.
+        } 
     printf("-----------------------------------------------------\n");
     printf("Result of Add\n");
-    printer(temp,row,col);
+    printer(temp,row,col); // printer() 로 2차원 배열 temp를 출력합니다.
     printf("-----------------------------------------------------\n");
-    free(temp);
+    free(temp); // 동적 메모리할당 된 temp를 메모리 해지합니다.
 }
 /////////////////////////////////////////////////////////////////////////
 
 ///////////////// (f) subtraction_matrix 구현 /////////////////
-void subtraction_matrix(int **array_a, int ** array_b,int row,int col){
-    int **temp = make_array(row,col);
+void subtraction_matrix(int **array_a, int ** array_b,int row,int col){ //함수의 매개변수로 이중 포인터 변수인 행렬 A와 행렬 B를 받고 행과 열의 크기를 받습니다.
+    int **temp = make_array(row,col); // 새로운 이중 포인터 temp를 행렬 생성함수 make_array로 초기화 합니다.
     for (int i=0;i<row;++i){ // 행을 탐색하기 위한 for문
         for (int j=0;j<col;++j) // 열을 탐색하기 위한 for문
-            temp[i][j] = array_a[i][j] - array_b[i][j]; // 1~9중 랜덤 값을 array[i][[j]의 값으로 초기화
-    }
+            temp[i][j] = array_a[i][j] - array_b[i][j]; // 행렬의 각각의 요소의 서로 동일한 인덱스를 공유하는 요소와 -연산을 합니다. 그 결과를 2차원 배열 temp에 저장합니다.
+     }
     printf("-----------------------------------------------------\n");
     printf("Result of Sub\n");
-    printer(temp,row,col);
+    printer(temp,row,col); // printer() 로 2차원 배열 temp를 출력합니다.
     printf("-----------------------------------------------------\n");
-    free(temp);
+    free(temp); // 동적 메모리할당 된 temp를 메모리 해지합니다.
 }
 /////////////////////////////////////////////////////////////////////////
 
 ///////////////// (g) transpose_matrix 구현 /////////////////
-void transpose_matrix(int **array,int row,int col){
-    int **temp = make_array(row,col);
+void transpose_matrix(int **array,int row,int col){ // 이중 포인터 변수 1개(행렬 A)와 행과 열의 값을 매개변수로 받습니다.
+    int **temp = make_array(col,row); // 새로운 이중 포인터 temp를 행렬 생성함수 make_array로 초기화 합니다.
     for (int i=0;i<row;++i){ // 행을 탐색하기 위한 for문
         for (int j=0;j<col;++j) // 열을 탐색하기 위한 for문
-            temp[i][j] = array[j][i]; // 1~9중 랜덤 값을 array[i][[j]의 값으로 초기화
+            temp[j][i] = array[i][j]; // 행렬 A의 행과 열의 요소를 열->행 순으로 temp에 넣어줍니다.
     }
     printf("-----------------------------------------------------\n");
     printf("Result of Transpose of A\n");
-    printer(temp,row,col);
+    printer(temp,col,row); //printer() 로 2차원 배열 temp를 출력합니다. // transpose이기에 행과 열의 차원이 바뀝니다.
     printf("-----------------------------------------------------\n");
-    free(temp);
+    free(temp); // 동적 메모리할당 된 temp를 메모리 해지합니다.
 }
 /////////////////////////////////////////////////////////////////////////
 
 ///////////////// (h) multiply_matrix 구현 /////////////////
-void multiply_matrix(int **array_a, int ** array_b,int row,int col){
-    int **temp = make_array(row,col);
-    for (int i=0;i<row;++i){ 
-        for (int j=0;j<col;++j){
-            int value = 0;
-            for (int k=0;k<col;++k){
-            value += array_a[i][k] * array_b[k][j]; 
+void multiply_matrix(int **array_a, int ** array_b,int row,int col){ // 매개변수로 이중 포인터 변수 두개와 행과 열의 값을 받습니다.
+    int **temp = make_array(row,col); // 새로운 이중 포인터 temp를 행렬 생성함수 make_array로 초기화 합니다.
+    for (int i=0;i<row;++i){  // 행을 탐색하기 위한 for문
+        for (int j=0;j<col;++j){ // 열을 탐색하기 위한 for문
+            int value = 0; // 행렬의 곱 연산에서 각 원소의 곱을 저장할 변수 value
+            for (int k=0;k<col;++k){ // 열의 갯수만 큼 k를 반복
+            value += array_a[i][k] * array_b[k][j]; // i와 j를 고정했을 때 k를 a에서는 수평방향 b에서는 수직방향으로 움직이게 하여 행렬의 곱셈 연산을 구현 하였습니다.
             }
-            temp[i][j] = value;
+            temp[i][j] = value; // 곱의 결과를 2차원 배열 temp에 저장
         }
     }
     printf("-----------------------------------------------------\n");
     printf("Result of Mul\n");
-    printer(temp,row,col);
+    printer(temp,row,col); //printer() 로 2차원 배열 temp를 출력합니다. 
     printf("-----------------------------------------------------\n");
-    free(temp);
+    free(temp); // 동적 메모리할당 된 temp를 메모리 해지합니다.
 }
 /////////////////////////////////////////////////////////////////////////
 
 
 ///////////////// (j) 메모리 해제 함수 /////////////////
-void free_matrix(int **array){
-    free(array);
+void free_matrix(int **array){ // 매개변수로 이중 포인터를 받습니다.
+    free(array); // free 함수로 동적할당 된 array를 해지합니다.
 }
 /////////////////////////////////////////////////////////////////////////
 
@@ -138,18 +154,21 @@ int main(){
     scanf("%d %d",&row_num,&column_num); // 행렬 A의 행과 열의 갯수 입력 받기
     /////////////////////////////////////////////////////////////////////////
 
-    int **array_a = make_array(row_num,column_num);
-    int **array_b = make_array(row_num,column_num);
+    int **array_a = make_array(row_num,column_num); // make_array 함수로 입력 받은 행과 열의 크기만큼의 A행렬 생성
+    int **array_b = make_array(row_num,column_num); // make_array 함수로 입력 받은 행과 열의 크기만큼의 B행렬 생성
 
-    put_random(array_a,row_num,column_num);
-    put_random(array_b,row_num,column_num);
+    put_random(array_a,row_num,column_num); // put_random 함수로 A행렬을 랜덤값으로 초기화
+    put_random(array_b,row_num,column_num); // put_random 함수로 B행렬을 랜덤값으로 초기화
 
 
-    print_matrix(array_a,array_b,row_num,column_num);
-    additon_matrix(array_a,array_b,row_num,column_num);
-    subtraction_matrix(array_a,array_b,row_num,column_num);
-    transpose_matrix(array_a,row_num,column_num);
-    multiply_matrix(array_a,array_b,row_num,column_num);
+    print_matrix(array_a,array_b,row_num,column_num); // A행렬과 B행렬을 출력합니다.
+    additon_matrix(array_a,array_b,row_num,column_num); // A행렬과 B행렬의 덧셈 결과를 출력합니다.
+    subtraction_matrix(array_a,array_b,row_num,column_num); // A행렬과 B행렬의 -연산 결과를 출력합니다.
+    transpose_matrix(array_a,row_num,column_num); // A행렬의 전치 행렬을 출력합니다.
+    multiply_matrix(array_a,array_b,row_num,column_num); // A행렬과 B행렬의 곱의 결과를 출력합니다.
+
+    free_matrix(array_a); // 동적 메모리 할당된 A행렬을 메모리 해지합니다.
+    free_matrix(array_b); // 동적 메모리 할당된 B행렬을 메모리 해지합니다.
 
     return 0;
 }
