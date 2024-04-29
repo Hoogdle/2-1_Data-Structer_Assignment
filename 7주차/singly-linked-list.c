5d76ad13a7d4b648c2fdd7d845986c95245b0645
Taeyeong Kim <xodud7737@gmail.com>
2024-04-29 23:23:28 +0900

    Assingment

--- 7주차/singly-linked-list.c
+++ 7주차/singly-linked-list.c
@@ -21,6 +21,8 @@ int insertNode(Node ** node,int data){
 
     if(!(*node)){ // node가 비어있는 경우
         *node = temp;
+        free(temp);
+        free(mover);
         return 0;
     }
 
@@ -37,6 +39,8 @@ int insertNode(Node ** node,int data){
         mover->next = temp;
         temp->next = messenger;
     }
+    free(temp);
+    free(mover);
     
 }
 
@@ -48,6 +52,8 @@ int insertFirst(Node ** node,int data){
     messenger = (*node)->next;
     (*node)->next = temp;
     temp ->next = messenger;
+    free(temp);
+    free(messenger);
 }
 
 int insertLast(Node ** node,int data){
@@ -60,6 +66,8 @@ int insertLast(Node ** node,int data){
     }
     mover->next = temp;
     temp->next = NULL;
+    free(temp);
+    free(mover);
 }
 
 int inverList(Node ** node){
@@ -82,50 +90,116 @@ int inverList(Node ** node){
         *node = b_head;
         (*node)->next = s_head;
     }
+    free(b_head);
+    free(s_head);
     return 0;
 }
 
 void deleteNode(Node **node,int data){
-    Node* temp;
-    temp = (Node*)malloc(sizeof(Node));
-    temp -> data = data;
 
     Node* mover = *node;
     Node* b_mover = NULL;
 
-    if(!(*node)){ // node가 비어있는 경우
-        *node = temp;
-        return 0;
-    }
+   
     while(mover->next != NULL && mover->data != data){ // mover 움직이기
         b_mover = mover;
         mover = mover->next;
     }
     if(mover->next != NULL){ // 마지막인경우
         if(mover->data != data){
+            free(mover);
+            free (b_mover);
             return;
         }
     }
-    else{
-        
+    else{ // 마지막이 아닌 경우, 찾는 data를 찾은 경우
+        b_mover->next = mover->next;
+        mover -> next = NULL;
+        free(mover);
+        free (b_mover);
     }
 }
 
-void deleteLast(Node **node,int data){
-
+void deleteLast(Node **node){
+    Node* mover = *node;
+    Node* b_mover = NULL;
+    while(mover->next != NULL){
+        b_mover = mover;
+        mover = mover->next;
+    }
+    b_mover->next = NULL;
+    free(mover);
+    free(b_mover);
 }
 
-void deleteFirst(Node **node,int data){
+void deleteFirst(Node **node){
+    Node *temp = (*node);
+    (*node) = (*node)->next;
+    temp->next = NULL;
 
+    free(temp);
 }
 
 
 void Print(Node *node){
-
+    int count = 0;
+    while(node != NULL){
+        printf("[ [%d] = %d ]\n",count,node->data);
+        ++count;
+        node = node->next;
+    }
 }
+
+
 
 
 int main(){
     Node* Head;
-    Head = (Node*)malloc(sizeof(Node));
+    char c;
+    do{
+        printf("        Please Input Command        \n");
+        printf("====================================\n");
+        printf("I : Initialize      P : Print\n");
+        printf("i : Insert Node     d : Delete Node\n");
+        printf("l : Insert Last     e : Delete Last\n");
+        printf("f : Insert First    x : Delete First\n");
+        printf("r : Invert List     q : Quit\n");
+        printf("====================================\n");
+        printf("Command : ");
+        scanf("%c",&c);
+
+        if(c=='I'){
+            Head = initialize();
+            continue;
+        }
+        else if(c=='i'){
+            int data;
+            printf("Input Data : ");
+            scanf("%d",&data);
+            insertNode(&Head,data);
+            continue;
+        }
+        else if(c=='l'){
+            int data;
+            printf("Input Data : ");
+            scanf("%d",&data);
+            insertLast(&Head,data);
+            continue;
+        }
+        else if(c=='f'){
+            int data;
+            printf("Input Data : ");
+            scanf("%d",&data);
+            insertFirst(&Head,data);
+            continue;
+        }
+        else if(c=='r'){
+            inverList(&Head);
+            continue;
+        }
+        else if(c=='P'){
+            
+        }
+    }while(c != 'q');
+ 
 }
