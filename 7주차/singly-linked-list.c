7ccd518f9177cb9a5869b9147300498afbcb667b
Taeyeong Kim <xodud7737@gmail.com>
2024-04-30 12:08:15 +0900

    Assignment

--- 7주차/singly-linked-list.c
+++ 7주차/singly-linked-list.c
@@ -8,7 +8,7 @@ typedef struct Node{
 
 Node* initialize(){
     Node* temp;
-    temp = (Node*)malloc(sizeof(Node));
+    temp = NULL;
     return temp;
 }
 
@@ -16,49 +16,60 @@ int insertNode(Node ** node,int data){
     Node* temp;
     temp = (Node*)malloc(sizeof(Node));
     temp -> data = data;
+    temp -> next = NULL;
 
     Node* mover = *node;
+    Node* tail = NULL;
 
     if(!(*node)){ // node가 비어있는 경우
         *node = temp;
-        free(temp);
-        free(mover);
         return 0;
     }
 
     while(mover->next != NULL && mover->data < data){
+        tail = mover;
         mover = mover->next;
     }
-    if(mover->next != NULL){ // 마지막인경우
-        mover->next = temp;
-        temp->next = NULL;
+    if(mover->data >= data){
+        if(tail==NULL){ // 첫 번째에 삽입
+            tail = (*node);
+            (*node) = temp;
+            temp->next = tail;
+        }
+        else{ // 중간에 삽입
+            tail->next = temp;
+            temp->next = mover;
+        }
     }
-    else{ // 중간인경우
-        Node* messenger;
-        messenger = mover->next;
+    else if(mover->next == NULL){ // 마지막인경우
         mover->next = temp;
-        temp->next = messenger;
     }
-    free(temp);
-    free(mover);
-    
 }
 
 int insertFirst(Node ** node,int data){
+
+    
     Node* temp;
     Node* messenger;
     temp = (Node*)malloc(sizeof(Node));
     temp -> data = data;
-    messenger = (*node)->next;
-    (*node)->next = temp;
+    temp -> next = NULL;
+
+    if(!(*node)){ // node가 비어있는 경우
+        *node = temp;
+        return 0;
+    }
+
+
+    messenger = (*node);
+    (*node) = temp;
     temp ->next = messenger;
-    free(temp);
-    free(messenger);
 }
 
 int insertLast(Node ** node,int data){
     Node* temp;
     temp = (Node*)malloc(sizeof(Node));
+    temp->data = data;
 
     Node* mover = *node;
     while(mover->next != NULL){
@@ -66,32 +77,32 @@ int insertLast(Node ** node,int data){
     }
     mover->next = temp;
     temp->next = NULL;
-    free(temp);
-    free(mover);
 }
 
-int inverList(Node ** node){
+int invertList(Node ** node){
     Node* b_head;
     Node* s_head;
+    Node* temp;
 
-    b_head = (*node);
-    s_head = NULL;
+    s_head = *node;
+    b_head = (*node)->next;
+    temp = b_head->next;
 
-    s_head = b_head;
-    b_head = b_head->next;
     s_head -> next = NULL;
-    *node = b_head;
-    (*node)->next = s_head;
+    b_head->next = s_head;
 
-    while(b_head->next != NULL){
+    while(1){
         s_head = b_head;
-        b_head = b_head->next;
+        b_head = temp;
+        if(!(b_head)){
+            break;
+        }
+        temp = temp->next;
         
-        *node = b_head;
-        (*node)->next = s_head;
+        b_head->next = s_head;
     }
-    free(b_head);
-    free(s_head);
+    *node = s_head;
+
     return 0;
 }
 
@@ -107,16 +118,12 @@ void deleteNode(Node **node,int data){
     }
     if(mover->next != NULL){ // 마지막인경우
         if(mover->data != data){
-            free(mover);
-            free (b_mover);
             return;
         }
     }
     else{ // 마지막이 아닌 경우, 찾는 data를 찾은 경우
         b_mover->next = mover->next;
         mover -> next = NULL;
-        free(mover);
-        free (b_mover);
     }
 }
 
@@ -128,8 +135,6 @@ void deleteLast(Node **node){
         mover = mover->next;
     }
     b_mover->next = NULL;
-    free(mover);
-    free(b_mover);
 }
 
 void deleteFirst(Node **node){
@@ -143,10 +148,13 @@ void deleteFirst(Node **node){
 
 void Print(Node *node){
     int count = 0;
-    while(node != NULL){
+    while(1){
         printf("[ [%d] = %d ]\n",count,node->data);
         ++count;
         node = node->next;
+        if(node==NULL){
+            return;
+        }
     }
 }
 
@@ -166,7 +174,7 @@ int main(){
         printf("r : Invert List     q : Quit\n");
         printf("====================================\n");
         printf("Command : ");
-        scanf("%c",&c);
+        scanf(" %c",&c);
 
         if(c=='I'){
             Head = initialize();
@@ -194,12 +202,32 @@ int main(){
             continue;
         }
         else if(c=='r'){
-            inverList(&Head);
+            invertList(&Head);
             continue;
         }
         else if(c=='P'){
-            
+            Print(Head);
+            continue;
         }
-    }while(c != 'q');
+        else if(c=='d'){
+            int data;
+            printf("Input Data : ");
+            scanf("%d",&data);
+            deleteNode(&Head,data);
+            continue;
+        }
+        else if(c=='e'){
+            deleteLast(&Head);
+            continue;
+        }
+        else if(c=='x'){
+            deleteFirst(&Head);
+            continue;
+        }
+        else if(c=='q'){
+            printf("프로그램을 종료합니다.");
+            return 0;
+        }
+    }while(1);
  
 }
